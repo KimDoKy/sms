a4dd649246ee992b2f5c66aa24c6a8ea318ddb35
Kim DoKy <makingfunk0@gmail.com>
2017-03-04 15:39:43 +0900

    sms : sms test sand. Success

--- sms_test/sms.py
+++ sms_test/sms.py
@@ -7,4 +7,37 @@ ROOT_PATH = os.path.dirname(SMS_TEST_PATH)
 CONF_PATH = os.path.join(ROOT_PATH, '.conf')
 
 config = json.loads(open(os.path.join(CONF_PATH, 'settings_local.json')).read())
-print(config)
+
+import sys
+from sdk.api.message import Message
+from sdk.exceptions import CoolsmsException
+
+##  @brief This sample code demonstrate how to send sms through CoolSMS Rest API PHP
+if __name__ == "__main__":
+
+    # set api key, api secret
+    api_key = config["sms"]["api_key"]
+    api_secret = config["sms"]["api_secret"]
+
+    ## 4 params(to, from, type, text) are mandatory. must be filled
+    params = dict()
+    params['type'] = 'sms' # Message type ( sms, lms, mms, ata )
+    params['to'] = '01084941303' # Recipients Number '01000000000,01000000001'
+    params['from'] = config["sms"]["sender_number"]
+    params['text'] = 'sms test message' # Message
+    cool = Message(api_key, api_secret)
+
+    try:
+        response = cool.send(params)
+        print("Success Count : %s" % response['success_count'])
+        print("Error Count : %s" % response['error_count'])
+        print("Group ID : %s" % response['group_id'])
+
+        if "error_list" in response:
+            print("Error List : %s" % response['error_list'])
+
+    except CoolsmsException as e:
+        print("Error Code : %s" % e.code)
+        print("Error Message : %s" % e.msg)
+
+    sys.exit()
