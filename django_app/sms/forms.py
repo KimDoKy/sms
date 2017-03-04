613a25b9c1f0a51cdbdb29404e84ea48bd2293ea
Kim DoKy <makingfunk0@gmail.com>
2017-03-04 18:15:55 +0900

    SMS : recipient numbers 검증

--- django_app/sms/forms.py
+++ django_app/sms/forms.py
@@ -1,4 +1,8 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
+
 
 class SMSForm(forms.Form):
     recipient_numbers = forms.CharField(
@@ -10,3 +14,16 @@ class SMSForm(forms.Form):
         )
     )
     content = forms.CharField(widget=forms.Textarea())
+
+    def clean_recipient_numbers(self):
+        cleaned_numbers = []
+        p = re.compile(r'^0\d{9}\d?')
+        number_string = self.cleaned_data['recipient_numbers']
+        numbers = number_string.replace('-', '').replace(' ', '').split(',')
+        for number in numbers:
+            if re.match(p, number):
+                cleaned_numbers.append(number)
+            else:
+                pass
+                # raise ValidationError('Invalid phone number format! {}'.format(number))
+            return cleaned_numbers
--- django_app/sms/views.py
+++ django_app/sms/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import SMSForm
@@ -6,10 +7,13 @@ from .forms import SMSForm
 def index(request):
     if request.method == 'POST':
         form = SMSForm(request.POST)
+        if form.is_valid():
+            numbers = form.cleaned_data['recipient_numbers']
+            return HttpResponse(numbers)
     else:
         form = SMSForm(
             initial={
-                'recipient_numbers': '010-8494-1303, 010-1234-1234, 0212341234'
+                'recipient_numbers': '010-8494-1303, 0212341234, 0123231'
             }
         )
     content = {
